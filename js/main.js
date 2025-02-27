71267fb3b8badc090ccc6814c34531d711a0c547
Alejandro Pabon <alejandropabon846@gmail.com>
2025-02-27 12:58:59 -0500

    feat: :construction: Se genera la estructura JS

---
+++ js/main.js
@@ -0,0 +1,38 @@
+import submit from "./components/type-order.js";
+const article__product = document.querySelector(".article_product");
+const products = [ 
+    {
+        image: "",
+        name: "Mix Ice Cream",
+        "type-order": [ 
+            "on Table",
+            "Delivery"
+        ],
+        price: [ 
+            199, 0
+        ]
+    },
+    {
+        image: "",
+        name: "Chocolate Cup",
+        "type-order": [ 
+            "on Table",
+            "Delivery"
+        ],
+        price: [ 
+            129, 2
+        ]
+    },
+    {
+        image: "",
+        name: "Barry Ice Cream",
+        "type-order":"on Table",
+        price: 109
+    },
+    {
+        image: "",
+        name: "Strawberry Ice cream",
+        "type-order": "Delivery",
+        price: 149
+    },
+];
